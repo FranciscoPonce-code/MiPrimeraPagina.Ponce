1bce7bd1ba5655f2f92f54343c4614835041bf61
Francisco Ponce <franciscoponce40@gmail.com>
2025-03-12 20:03:41 -0300

    Agregados modelos Plato, Reserva y Cliente

--- miapp/models.py
+++ miapp/models.py
@@ -1,3 +1,30 @@
-from django.db import models
+from django.db import models  # noqa: F401
 
 # Create your models here.
+
+from django.db import models  # noqa: F811
+
+class Plato(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
+
+class Reserva(models.Model):
+    nombre = models.CharField(max_length=100)
+    fecha = models.DateField()
+    hora = models.TimeField()
+    personas = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nombre
