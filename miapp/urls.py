b7168bd8631f5c7cb564b4c2e5a238fcfef9b409
Francisco Ponce <franciscoponce40@gmail.com>
2025-03-12 21:56:45 -0300

    Agregadas URLs para la aplicación

--- miapp/urls.py
+++ miapp/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from . import views  # Importa las vistas de la app
+from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Asegúrate de que "home" esté definido en views.py
+    path('', views.index, name='index'),
+    path('menu/', views.menu, name='menu'),
+    path('reservas/', views.reservas, name='reservas'),
+    path('nuevo_plato/', views.nuevo_plato, name='nuevo_plato'),
+    path('nuevo_cliente/', views.nuevo_cliente, name='nuevo_cliente'),
+    path('buscar/', views.buscar_plato, name='buscar_plato'),
 ]
